e68f201cc808db95e0e9d96762cb765ff40db84e
Jason Smythe <jason@smythemail.za.net>
2017-03-24 01:44:52 +0200

    Working (Well not really) Server request.

--- src/containers/LeftPane.js
+++ src/containers/LeftPane.js
@@ -4,6 +4,20 @@ import { changeTab } from '../actions'
 import '../index.css'
 
 const LeftPane  = ({ loadedString, changeTab, tab }) => {
+  const getData = () =>{
+    let form = new FormData()
+    form.set('xml', loadedString)
+    fetch('http://attempto.ifi.uzh.ch/service/owl_verbalizer/owl_to_ace', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/text',
+      },
+      body: form
+    }).then(function(response) {
+      console.log(response.type)
+    })
+  }
   const displayPane = () => {
     switch (tab) {
       case 'inputOwl':
@@ -14,7 +28,12 @@ const LeftPane  = ({ loadedString, changeTab, tab }) => {
         </div>
       case 'attempto':
         return <div className="scroll-box">
-          Attempto tab
+          <p>This functionality doesn't work.</p>
+          <p>After spending hours and hours, and more hours.</p>
+          <p>Trying to get the <a href='https://github.com/Kaljurand/owl-verbalizer'>server</a> to allow cross-origin requests.</p>
+          <a href='http://stackoverflow.com/a/41921909/3103033'>See here for the issue</a>.
+          <p>Please 'inspect' the page to see the 'Network' tab for the response to this request.</p>
+          <button onClick={getData}>Load own verbalisation</button>
         </div>
       case 'editor':
           return <div className="scroll-box">
